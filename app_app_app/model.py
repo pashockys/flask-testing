6eb19c8beadd6be2ae0e3a57a82b346cba0b721f
pashockys <pashockys92@mail.ru>
2021-07-29 22:44:32 +0700

    sas

--- app_app_app/model.py
+++ app_app_app/model.py
@@ -51,6 +51,14 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+               followers.c.follower_id == self.id)
+        own_posts = Post.query.filter_by(user_id=self.id)
+        return followed.union(own_posts).order_by(Post.timestamp.desc())
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
